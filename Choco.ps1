93be9e753e8ea39c82206d6e11dd9e0af4118436
vikekh <621212+vikekh@users.noreply.github.com>
2020-06-09 00:57:58 +0200

    Added Choco.ps1

---
+++ Choco.ps1
@@ -0,0 +1,12 @@
+#choco install anydvd
+choco install duplicati
+choco install google-backup-and-sync
+choco install imgburn
+choco install kdiff3
+choco install makemkv
+choco install paint.net
+choco install plex
+choco install samsung-magician
+choco install treesizefree
+choco install veracrypt
+choco install visualstudio2019community-preview --pre 
